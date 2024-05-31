c4796ad88d196d9c6096313bf88a1409f6749179
DaianePorcena <daiporcenna@gmail.com>
2024-05-31 13:45:47 -0300

    ✨ feat: Add Java project

---
+++ backend/src/main/java/com/daiane/starcatcherportfolio/StarcatcherportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.daiane.starcatcherportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarcatcherportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarcatcherportfolioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/daiane/starcatcherportfolio/StarcatcherportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daiane.starcatcherportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarcatcherportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
