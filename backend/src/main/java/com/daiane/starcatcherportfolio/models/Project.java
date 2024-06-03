6e6afcb63d8481fb6f2fd47d790b2cbd6546b32f
DaianePorcena <daiporcenna@gmail.com>
2024-06-03 09:27:50 -0300

    ✨ feat: Add Project model class

---
+++ backend/src/main/java/com/daiane/starcatcherportfolio/models/Project.java
@@ -0,0 +1,139 @@
+package com.daiane.starcatcherportfolio.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "project")
+public class Project {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", unique = true)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false, updatable = false)
+  private User user;
+
+  @Column(name = "title", nullable = false, length = 255)
+  @NotNull
+  @NotEmpty
+  @Size(min = 3, max = 255)
+  private String title;
+
+  @Column(name = "description", nullable = false, length = 255)
+  @NotNull
+  @NotEmpty
+  @Size(min = 3, max = 255)
+  private String description;
+
+  @Column(name = "url_link", nullable = false)
+  @NotNull
+  @NotEmpty
+  private String urlLink;
+
+  public Project() {
+  }
+
+  public Project(Long id, User user, String title, String description, String urlLink) {
+    this.id = id;
+    this.user = user;
+    this.title = title;
+    this.description = description;
+    this.urlLink = urlLink;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getUrlLink() {
+    return urlLink;
+  }
+
+  public void setUrlLink(String urlLink) {
+    this.urlLink = urlLink;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((title == null) ? 0 : title.hashCode());
+    result = prime * result + ((description == null) ? 0 : description.hashCode());
+    result = prime * result + ((urlLink == null) ? 0 : urlLink.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Project other = (Project) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (title == null) {
+      if (other.title != null)
+        return false;
+    } else if (!title.equals(other.title))
+      return false;
+    if (description == null) {
+      if (other.description != null)
+        return false;
+    } else if (!description.equals(other.description))
+      return false;
+    if (urlLink == null) {
+      if (other.urlLink != null)
+        return false;
+    } else if (!urlLink.equals(other.urlLink))
+      return false;
+    return true;
+  }
+
+}
